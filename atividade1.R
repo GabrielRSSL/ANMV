29666c73e4b34fd562908ce5e96c1a743be6be51
GabrielRSSL <gabriel3oliveira7@gmail.com>
2026-08-11 14:14:44 -0300

    Create atividade1.R

---
+++ atividade1.R
@@ -0,0 +1,12 @@
+# 1.
+
+produto <- c("Arroz", "Feijão", "Macarrão")
+preco <- c(15.50, 8.20, 5.00)
+
+loja_vendas <- data.frame(produto, preco)
+View(loja_vendas)
+
+# i.
+# ii.
+
+loja_vendas[2, 3] <- 
